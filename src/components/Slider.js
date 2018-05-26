7dd6d2ceb311e7dd02c7d4ea465aabecbc2c1d7c
Filip Stepien <contact@filipstepien.com>
2018-05-25 21:11:57 -0400

    add exchange api call, filter out all productrs with price = 0, convert all prices to CAD cents, calc max, min avg and add to state

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 
 const Slider = props => {
   return (
     <div>
       <p>Budget: ${props.budget.range} </p>
-        <span>
-            {props.budget.min}
-            <input
-                type="range"
-                min={props.budget.min}
-                max={props.budget.max}
-                onChange={e => {
-                props.setBudget(e.target.value);
-                  }} />
-            {props.budget.max}
-        </span>
-        
+      <span>
+        {props.budget.min}
+        <input
+          type="range"
+          min={props.budget.min}
+          max={props.budget.max}
+          onChange={e => {
+            props.setBudget(e.target.value);
+          }}
+        />
+        {props.budget.max}
+      </span>
     </div>
   );
 };
