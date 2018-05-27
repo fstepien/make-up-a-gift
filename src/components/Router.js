03e26823a434000022169b9f38c737f535d59227
Zena Jun <zena.jun@gmail.com>
2018-05-27 11:51:18 -0400

    PersonalGift component made

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from '../App.js'
 import SendEmail from './SendEmail';
+import PersonalGift from './PersonalGift.js';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
-      <Route path="/your-gift/:item1/:item2/:item3" component={SendEmail} />
+      <Route path="/your-gift/:item1/:item2/:item3" component={PersonalGift} />
     </Switch>
   </BrowserRouter>
 )
