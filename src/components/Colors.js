b4f50021131b3d8e4886b86594b99fa96ebdf489
Lucas Knight <lucas.a.knight@outlook.com>
2018-05-29 00:24:02 -0400

    colours

---
+++ src/components/Colors.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Colors = (props) => {
+    console.log(props)
+    const {colors} = props;
+    const colorItem = colors.map(color => (
+        <li className="color" style={{ background: color.hex_value, color: color.hex_value }}>
+        <div className="color"/>
+      </li>
+    ));
+    
+    return( 
+        <ul className="colorList">{colorItem}</ul>
+    )
+}
+
+export default Colors;
