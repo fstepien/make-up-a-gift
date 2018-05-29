d7c89f1eacd912d16aaae5e297e980e453c87c01
Lucas Knight <lucas.a.knight@outlook.com>
2018-05-29 16:52:22 -0400

    Lock and Unlock btns styled with different SVGs

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -3,16 +3,15 @@ import Colors from "./Colors";
 
 
 const ProductItem = (props) => {
-    console.log(props)
 
     const {product, productNum, toggleLock} = props;
+
     return(
     <React.Fragment>
-        <img src="/assets/lock.svg" alt=""/>
-        <div style={{background: "url(../public/assets/lock.svg)", width: '200px'}}>.</div>
         <input 
             type="image" 
-            src="../public/assets/unlock.svg"
+            src={props.locked ? "/assets/lock.svg" : "/assets/unlock.svg"}
+            className="productBtn"
             onClick={() => props.toggleLock(product.product_type, product.id)} />
         <h3>{product.name.replace(/\b\w/g, l => l.toUpperCase())}</h3>
         <img src={product.api_featured_image} className="displayImg" />
