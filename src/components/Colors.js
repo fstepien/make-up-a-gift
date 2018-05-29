8ec30fe1e116c9f454ae1343000a2a15b78aca79
Filip Stepien <contact@filipstepien.com>
2018-05-29 17:49:23 -0400

    add lock functionality within app

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -1,17 +1,19 @@
 import React from "react";
 
-const Colors = (props) => {
-    console.log(props)
-    const {colors} = props;
-    const colorItem = colors.map(color => (
-        <li className="color" style={{ background: color.hex_value, color: color.hex_value }}>
-        <div className="color"/>
-      </li>
-    ));
-    
-    return( 
-        <ul className="colorList">{colorItem}</ul>
-    )
-}
+const Colors = props => {
+  console.log(props);
+  const { colors } = props;
+  const colorItem = colors.map((color, i) => (
+    <li
+      key={i}
+      className="color"
+      style={{ background: color.hex_value, color: color.hex_value }}
+    >
+      <div className="color" />
+    </li>
+  ));
+
+  return <ul className="colorList">{colorItem}</ul>;
+};
 
 export default Colors;
