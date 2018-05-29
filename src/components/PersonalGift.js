d43f4edf0e0b9d234da497a50f991015046236b5
Zena Jun <zena.jun@gmail.com>
2018-05-29 12:21:43 -0400

    Personal Gift is functional

--- src/components/PersonalGift.js
+++ src/components/PersonalGift.js
@@ -1,77 +1,84 @@
 import React, { Component } from "react";
 import axios from 'axios';
-import { get } from "http";
 
 // http://localhost:3000/your-gift/blush-920/eyeliner-247/lipstick-885
 
 class PersonalGift extends Component {
   state = {
-    products: {}
+    ids: [],
+    products: []
   };
   
   componentDidMount() {
-    // Get products and ids from the URL
-    const products = { ...this.state.products }; // a copy of the state and child(ren). 
+    // Get ids from the URL    
+    const ids = this.state.ids
+    const item1 = parseInt(this.props.match.params.item1, 10);
+    const item2 = parseInt(this.props.match.params.item2, 10);
+    const item3 = parseInt(this.props.match.params.item3, 10);
+    ids.push(item1, item2, item3)
 
-    const product1 = this.props.match.params.product1;
-    const item1 = parseInt(this.props.match.params.item1);
-    products[product1] = product1;
-    products[product1] = item1;      
-       
-    const product2 = this.props.match.params.product2;
-    const item2 = parseInt(this.props.match.params.item2);
-    products[product2] = product2;
-    products[product2] = item2;
-    
-    const product3 = this.props.match.params.product3;
-    const item3 = parseInt(this.props.match.params.item3);
-    products[product3] = product3;  
-    products[product3] = item3;
-
-    this.setState({ products }, () => {
-      for (let product in products) {      
-        const id = products[product];       
-        this.getProducts(product, id)           
-      }      
+    this.setState({ ids }, () => {
+      const products = this.state.products;      
+        
+      ids.map((id) => {          
+        this.getProducts(products, id)
+      }); 
+      // this.setState({ products });             
     });    
   }
   
-  getProducts = (product, id) => {    
+  getProducts = (products, id) => { 
     axios({
       method: "GET",
-      url: `https://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product}`,
+      url: `https://makeup-api.herokuapp.com/api/v1/products/${id}.json`,
       responseType: "json"
     })
-      .then(res => {  
-
-        const products = {...this.state.product }    
-        products[product] = res.data; 
-        console.log(products[product]);
-        products[product].filter((item) => {
-         if (item.id === id) {
-           const product = item;
-           products[product] = item     
-           
-                           
-           this.setState({ products })
-          }
-          
-        })
-        
-      })
-      .catch(err => console.log(err));
+    .then(res => {     
+      // products.push(res.data);
+      const prodClone = Array.from(this.state.products);    
+      prodClone.push(res.data);
+      this.setState({
+        products: prodClone
+      })                
+    })
+    .catch(err => console.log(err));     
   }
 
+  renderProducts = (arrayToCopy) => {   
+    const newArray = Array.from(arrayToCopy);
 
+    let markup = [];
+    newArray.map((product) => {
+        markup.push(
+          <div className="product">
+            <p>{product.name} by - {product.brand.toUpperCase()}</p>
+            <img src={product.api_featured_image} alt={product.name} className="displayImg"/>
+            <a href={product.product_link} target="_blank">Buy me</a>
+          </div>
+        )
+      console.log(product.name);       
+    })
+    // products.map((product) => {
+    //   console.log(product);
+    // })
+    // console.log(markup);
+    
+    return markup;
+    
+    
+  
+
+    
+    
+  }
 
   render() {
     return (
-      <div className="your-gift">
-        <h2>Your Personal Gift</h2>
-        <p>Item 1, {this.props.match.params.product1} {this.props.match.params.item1} </p>
-        <p>Item 2, {this.props.match.params.product2} {this.props.match.params.item2} </p>
-        <p>Item 3, {this.props.match.params.product3} {this.props.match.params.item3} </p>
-
+      <div className="wrap clearfix">
+        <h2>Your Personal Gift</h2>      
+        <div>
+          {this.renderProducts(this.state.products)}
+        </div>
       </div>
     )
   }
