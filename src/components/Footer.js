2140a3095c64f2b82c7283c6a3976b8bafc3eceb
Zena Jun <zena.jun@gmail.com>
2018-05-29 15:18:07 -0400

    Added Footer component, styling incomplete

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="App-footer">
+    <div className="wrap">
+      <p>&copy; 2018 Made by Filip, Lucas & Zena</p>
+      <p>Built using the Makeup API</p>
+    </div>
+  </footer>
+)
+
+export default Footer;
+
+
+// 3e4040;
