330374865f24492b7a19b1a7ce9aeb3c0a3d7ec8
Zena Jun <zena.jun@gmail.com>
2018-05-29 22:28:26 -0400

    product display styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => (
   <footer className="App-footer">
     <div className="wrap">
       <p>&copy; 2018 Made by <a href="https://filipstepien.com/" target="_blank">Filip</a>, <a href="https://github.com/Armox" target="_blank">Lucas</a> & <a href="http://www.zenajun.com/" target="_blank">Zena</a></p>
-      <p>Built using the Makeup API</p>
+      <p>Built using the <a href="https://makeup-api.herokuapp.com/" target="_blank">Makeup API</a></p>
     </div>
   </footer>
 );
