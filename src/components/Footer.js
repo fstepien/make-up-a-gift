17416bc88225c8791c0905e3a86f7eeb90ee7cbe
Zena Jun <zena.jun@gmail.com>
2018-05-29 21:03:32 -0400

    Editing Footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import React from "react";
 const Footer = () => (
   <footer className="App-footer">
     <div className="wrap">
-      <p>&copy; 2018 Made by Filip, Lucas & Zena</p>
+      <p>&copy; 2018 Made by <a href="https://filipstepien.com/" target="_blank">Filip</a>, <a href="https://github.com/Armox" target="_blank">Lucas</a> & <a href="http://www.zenajun.com/" target="_blank">Zena</a></p>
       <p>Built using the Makeup API</p>
     </div>
   </footer>
-)
+);
 
 export default Footer;
